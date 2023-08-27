a2e86a12fc634b160f6f785de76ef385a59e127b
lqy231 <qinyulan0@gmail.com>
2023-08-27 16:04:09 +0800

    finish languages

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -2,4 +2,25 @@
 estimate time: 30 minutes
 actual time: 20 minutes
 """
+from prac_06.programming_language import ProgrammingLanguage
 
+
+def main():
+    """main function"""
+    languages = []
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    print(python)
+    print(ruby)
+    print(visual_basic)
+    languages.append(python)
+    languages.append(ruby)
+    languages.append(visual_basic)
+    print("The dynamically typed languages are:")
+    for language in languages:
+        if language.is_dynamic():
+            print(language.name)
+
+
+main()
