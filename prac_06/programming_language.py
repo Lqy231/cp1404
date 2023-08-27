7dea33cbeb516ded9228a8a99c424f266fb33a73
lqy231 <qinyulan0@gmail.com>
2023-08-27 15:51:07 +0800

    finish programlanguage class

---
+++ prac_06/programming_language.py
@@ -0,0 +1,17 @@
+
+class programing_language:
+    def __init__(self, name, typing, reflection, year):
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def __str__(self):
+        return f"{self.name}, {self.typing}, {self.reflection}, {self.year}"
+    
+    
+    def is_dynamic(self):
+        if self.typing == "Dynamic":
+            return True
+        else:
+            return False
