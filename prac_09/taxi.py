04bc9c46116a0789026e3652343234386d4e6d73
lqy231 <qinyulan0@gmail.com>
2023-09-15 22:55:39 +0800

    Initial commit taxi simulator

--- prac_09/taxi.py
+++ prac_09/taxi.py
@@ -1,8 +1,8 @@
 """
 CP1404/CP5632 Practical
-Car class
+Taxi class
 """
-from prac_09.car import Car
+from car import Car
 
 
 class Taxi(Car):
@@ -12,7 +12,6 @@ class Taxi(Car):
     def __init__(self, name, fuel):
         """Initialise a Taxi instance, based on parent class Car."""
         super().__init__(name, fuel)
-        # self.price_per_km = price_per_km
         self.current_fare_distance = 0
 
     def __str__(self):
@@ -21,7 +20,7 @@ class Taxi(Car):
 
     def get_fare(self):
         """Return the price for the taxi trip."""
-        return self.price_per_km * self.current_fare_distance
+        return round(self.price_per_km * self.current_fare_distance, 1)
 
     def start_fare(self):
         """Begin a new fare."""
