ef3a4220c1ac51421600528bdfe6c6db37b99e38
lqy231 <qinyulan0@gmail.com>
2023-09-24 01:45:17 +0800

    final commit wiki

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -9,7 +9,7 @@ search_phrase = input("Search phrase: ")
 while search_phrase != '':
     try:
         searched_page = wikipedia.page(search_phrase)
-        print(f"Title: {searched_page.title}\nSummary: {wikipedia.summary(search_phrase)}URL: {searched_page.url}\n")
+        print(f"Title: {searched_page.title}\nSummary: {wikipedia.summary(search_phrase)}\nURL: {searched_page.url}\n")
     except wikipedia.exceptions.DisambiguationError as e:
         print(e.options)
     search_phrase = input("Search phrase: ")
